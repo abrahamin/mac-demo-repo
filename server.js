e1847934a04a79c15b84036e29e1033161907d6e
abrahamin <abrahamin.html@gmail.com>
2022-01-17 18:40:50 -0500

    middleware

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
-
+const { middleware } = require('./middleware/middleware')
 const PORT = 3001;
+
+app.use(middleware);
 
 const app = express();
