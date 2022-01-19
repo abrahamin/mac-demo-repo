4bd187209534c211a41327efcdb91d4ee9d876d0
abrahamin <abrahamin.html@gmail.com>
2022-01-18 22:26:39 -0500

    mysql

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const { middleware } = require('./middleware/middleware')
+const db = require("./db");
 const PORT = 3001;
 
 app.use(middleware);
