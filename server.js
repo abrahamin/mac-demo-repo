0d6921dc0ec64f7a0b4552b843ff212d10b9bd55
abrahamin <abrahamin.html@gmail.com>
2022-01-19 21:26:29 -0500

    sequelize

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const sequelize = require('./config/connection')
+
 const path = require('path');
 const { middleware } = require('./middleware/middleware')
 const db = require("./db");
